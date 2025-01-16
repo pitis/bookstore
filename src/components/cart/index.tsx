dd4fff4c9103588cae9d5a0b6631239f18f29093
Pitis <pitis.radu@protonmail.com>
2025-01-16 18:22:03 +0200

    feat: cart component

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -1,7 +1,64 @@
+import useAppStore from '@/stores/cart'
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonText,
+  IonToolbar,
+} from '@ionic/react'
+
 export default function Cart() {
+  const { cart, addToCart, removeFromCart } = useAppStore()
+
   return (
-    <div>
-      <div>span</div>
+    <div style={{ width: '350px' }}>
+      <IonHeader>
+        <IonToolbar>Cart</IonToolbar>
+      </IonHeader>
+
+      {!cart.length && <IonText>Cart is empty</IonText>}
+      <IonList style={{ height: '100%' }}>
+        {cart?.map((cartItem) => (
+          <IonItem key={cartItem.id}>
+            <IonLabel class='ion-text-wrap'>
+              <h2>{cartItem.title}</h2>
+              <p>
+                <IonText color='medium'>Author: {cartItem.author}</IonText>
+              </p>
+              <p>
+                <IonText color='primary'>
+                  Price: ${cartItem.price.toFixed(2)}
+                </IonText>
+              </p>
+              <div
+                style={{ display: 'flex', alignItems: 'center', gap: '12px' }}>
+                <IonButton
+                  slot='end'
+                  color='primary'
+                  disabled={cartItem.stock === 0}
+                  onClick={() => removeFromCart(cartItem.id)}>
+                  -
+                </IonButton>
+
+                <p>
+                  <IonText color='primary'>{cartItem.quantity}</IonText>
+                </p>
+
+                <IonButton
+                  slot='end'
+                  color='primary'
+                  disabled={cartItem.stock === 0}
+                  onClick={() => addToCart(cartItem.id)}>
+                  +
+                </IonButton>
+              </div>
+            </IonLabel>
+          </IonItem>
+        ))}
+      </IonList>
     </div>
   )
 }
