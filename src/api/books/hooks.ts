e368dac98efef2630591fb9ed88d4fda7bfdd819
Pitis <pitis.radu@protonmail.com>
2025-01-16 17:21:39 +0200

    feat: filtering and debouncing

--- src/api/books/hooks.ts
+++ src/api/books/hooks.ts
@@ -1,11 +1,19 @@
 import { useQuery } from '@tanstack/react-query'
 import { mockData } from '@/utils/mockData'
 
-export function useGetBooks() {
+export function useGetBooks(searchText: string) {
   return useQuery({
-    queryKey: ['books'],
+    queryKey: ['books', searchText],
     queryFn: async () => {
-      return mockData
+      if (searchText === '') return mockData
+
+      const filteredBooks = mockData.filter(
+        (book) =>
+          book.title.toLowerCase().includes(searchText.toLowerCase()) ||
+          book.author.toLowerCase().includes(searchText.toLowerCase())
+      )
+
+      return filteredBooks
     },
     initialData: [],
   })
--- src/pages/books/index.tsx
+++ src/pages/books/index.tsx
@@ -2,42 +2,71 @@ import { useGetBooks } from '@/api/books/hooks'
 import {
   IonButton,
   IonContent,
+  IonHeader,
   IonItem,
   IonLabel,
   IonList,
+  IonSearchbar,
   IonText,
 } from '@ionic/react'
+import { useEffect, useState } from 'react'
 
 export default function Books() {
-  const { data: books } = useGetBooks()
+  const [searchText, setSearchText] = useState<string>('')
+  const [debouncedSearchText, setDebouncedSearchText] = useState<string>('')
+
+  const { data: books } = useGetBooks(debouncedSearchText)
+
+  useEffect(() => {
+    const debounce = setTimeout(() => {
+      setDebouncedSearchText(searchText)
+    }, 300)
+
+    return () => {
+      clearTimeout(debounce)
+    }
+  }, [searchText])
 
   return (
-    <IonContent>
-      <IonList>
-        {books.map((book) => (
-          <IonItem key={book.id}>
-            <IonLabel class='ion-text-wrap'>
-              <h2>{book.title}</h2>
-              <p>
-                <IonText color='medium'>Author: {book.author}</IonText>
-              </p>
-              <p>
-                <IonText color='primary'>
-                  Price: ${book.price.toFixed(2)}
-                </IonText>
-              </p>
-              <p>
-                <IonText color={book.stock > 0 ? 'success' : 'danger'}>
-                  {book.stock > 0 ? `In Stock: ${book.stock}` : 'Out of Stock'}
-                </IonText>
-              </p>
-            </IonLabel>
-            <IonButton slot='end' color='primary' disabled={book.stock === 0}>
-              Add me to cart
-            </IonButton>
-          </IonItem>
-        ))}
-      </IonList>
-    </IonContent>
+    <>
+      <IonHeader>
+        <IonSearchbar
+          placeholder='Search for a book...'
+          showClearButton='always'
+          value={searchText}
+          onIonInput={(e) => setSearchText(e.detail.value!)}
+        />
+      </IonHeader>
+
+      <IonContent>
+        <IonList>
+          {books.map((book) => (
+            <IonItem key={book.id}>
+              <IonLabel class='ion-text-wrap'>
+                <h2>{book.title}</h2>
+                <p>
+                  <IonText color='medium'>Author: {book.author}</IonText>
+                </p>
+                <p>
+                  <IonText color='primary'>
+                    Price: ${book.price.toFixed(2)}
+                  </IonText>
+                </p>
+                <p>
+                  <IonText color={book.stock > 0 ? 'success' : 'danger'}>
+                    {book.stock > 0
+                      ? `In Stock: ${book.stock}`
+                      : 'Out of Stock'}
+                  </IonText>
+                </p>
+              </IonLabel>
+              <IonButton slot='end' color='primary' disabled={book.stock === 0}>
+                Add me to cart
+              </IonButton>
+            </IonItem>
+          ))}
+        </IonList>
+      </IonContent>
+    </>
   )
 }
