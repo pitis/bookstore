822d901fff06649a70c4962fd2bfe06d8320be74
Pitis <pitis.radu@protonmail.com>
2025-01-16 17:10:44 +0200

    feat: show books

--- src/pages/books/index.tsx
+++ src/pages/books/index.tsx
@@ -1,7 +1,43 @@
+import { useGetBooks } from '@/api/books/hooks'
+import {
+  IonButton,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonText,
+} from '@ionic/react'
+
 export default function Books() {
+  const { data: books } = useGetBooks()
+
   return (
-    <div>
-      <span>books</span>
-    </div>
+    <IonContent>
+      <IonList>
+        {books.map((book) => (
+          <IonItem key={book.id}>
+            <IonLabel class='ion-text-wrap'>
+              <h2>{book.title}</h2>
+              <p>
+                <IonText color='medium'>Author: {book.author}</IonText>
+              </p>
+              <p>
+                <IonText color='primary'>
+                  Price: ${book.price.toFixed(2)}
+                </IonText>
+              </p>
+              <p>
+                <IonText color={book.stock > 0 ? 'success' : 'danger'}>
+                  {book.stock > 0 ? `In Stock: ${book.stock}` : 'Out of Stock'}
+                </IonText>
+              </p>
+            </IonLabel>
+            <IonButton slot='end' color='primary' disabled={book.stock === 0}>
+              Add me to cart
+            </IonButton>
+          </IonItem>
+        ))}
+      </IonList>
+    </IonContent>
   )
 }
